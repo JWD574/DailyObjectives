3082689987612ef63de0100fe97fd6770f756eed
rangerboy574 <jdeweese574@gmail.com>
2022-12-26 12:32:10 -0600

    Initial Commit

---
+++ DailyObjectives/DailyObjectivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyObjectivesApp.swift
+//  DailyObjectives
+//
+//  Created by Joseph  DeWeese on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyObjectivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
